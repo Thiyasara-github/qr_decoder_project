65fbb5b28f3acfc2c650992bdeae868e5cd750c3
Thevindu Vithanage <97927940+Thiyasara-github@users.noreply.github.com>
2024-12-21 17:15:58 +0530

    new changes

--- app.py
+++ app.py
@@ -34,21 +34,56 @@
 # if __name__ == '__main__':
 #     app.run(debug=True)
 
+# from flask import Flask, request, jsonify, render_template, send_file
+# from flask_cors import CORS
+# from qr_decoder import QRCodeDecoder
+# import os
+
+# app = Flask(__name__)
+# CORS(app)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/home')
+# def home():
+#     return send_file('Home.html')
+
+# @app.route('/decode-qr', methods=['POST'])
+# def decode_qr():
+#     try:
+#         data = request.get_json()
+#         base64_image = data.get('image')
+        
+#         if not base64_image:
+#             return jsonify({"error": "No image provided"}), 400
+        
+#         decoder = QRCodeDecoder()
+#         result = decoder.decode_qr_code_from_base64(base64_image)
+        
+#         return jsonify(result)
+    
+#     except Exception as e:
+#         return jsonify({"error": str(e)}), 500
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 from flask import Flask, request, jsonify, render_template, send_file
 from flask_cors import CORS
 from qr_decoder import QRCodeDecoder
-import os
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/home')
 def home():
-    return send_file('Home.html')
+    return send_file('index.html')
+
+@app.route('/code')
+def decoder():
+    return render_template('code.html')
 
 @app.route('/decode-qr', methods=['POST'])
 def decode_qr():
